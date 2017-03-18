bc05d44c86768a22e7ac9d191f2bc7b85c6908ca
varshavjm <varsha.vjm@gmail.com>
2017-03-18 14:31:10 -0700

    Custom fucntion for spatial join added

--- src/main/java/org/datasyslab/geospark/spatialOperator/JoinQuery.java
+++ src/main/java/org/datasyslab/geospark/spatialOperator/JoinQuery.java
@@ -645,4 +645,45 @@ public class JoinQuery implements Serializable{
             return resultCountByKey;
         }
     }
+		public static JavaPairRDD<Envelope, HashSet<Point>> getCustomSpatialJoinQuery(String pointRDD, String RectangleRDD)
+	{
+		
+		SparkConf conf = new SparkConf().setMaster("master").setAppName("JoinQuery");
+		final JavaSparkContext sc = new JavaSparkContext(conf);
+
+		PointRDD objectRDD = new PointRDD(sc, pointRDD, 0, FileDataSplitter.CSV, false, 10, StorageLevel
+				.MEMORY_ONLY_SER()); /*
+										 * The O means spatial attribute starts at
+										 * Column 0 and the 10 means 10 RDD
+										 * partitions
+										 */
+		RectangleRDD rectangleRDD = new RectangleRDD(sc, RectangleRDD, 0, FileDataSplitter.CSV, false,
+				StorageLevel
+						.MEMORY_ONLY_SER()); 
+		// collect rectangles into a java list
+		JavaRDD javaRDD = rectangleRDD.getRawSpatialRDD();
+		List<Envelope>rectangleRDDList=javaRDD.collect();
+		JavaRDD<Point> resultRDD = null;
+		List listOfPoints=new ArrayList<Point>();
+		List<Tuple2<Envelope, HashSet<Point>>>pointRectanglePairList=new ArrayList<Tuple2<Envelope, HashSet<Point>>>();
+		JavaPairRDD<Envelope, HashSet<Point>> resultList = null;
+		for (Envelope env : rectangleRDDList) {
+			try {
+				resultRDD = RangeQuery.SpatialRangeQuery(objectRDD, env, 0, false);
+				listOfPoints=resultRDD.collect();
+				HashSet resultPointSet=new HashSet<Point>(listOfPoints);
+				Tuple2 tuple=new Tuple2<Envelope, HashSet<Point>>(env,resultPointSet);
+				pointRectanglePairList.add(tuple);
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			resultList=sc.parallelizePairs(pointRectanglePairList);
+			return resultList;
+	}
+		return resultList;
+}
+
 }
